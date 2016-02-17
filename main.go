9f8e590232bee42bf23541784eb9525c1d74191d
Daisuke Fujita <dtanshi45@gmail.com>
2016-02-17 18:15:07 +0900

    Add submit endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +12,19 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"alert": false,
+		})
+	})
+
+	r.POST("/submit", func(c *gin.Context) {
+		username := c.PostForm("username")
+		pubKey := c.PostForm("pubKey")
+
+		c.HTML(http.StatusCreated, "index.tmpl", gin.H{
+			"alert":   true,
+			"message": strings.Join([]string{username, ": ", pubKey}, " "),
+		})
 	})
 
 	r.Run()
