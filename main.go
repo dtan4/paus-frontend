ad6ee04b17bd68b998a5bc3e6f0fa97a00b8dbab
Daisuke Fujita <dtanshi45@gmail.com>
2016-02-17 20:57:44 +0900

    Use COMPOSE_FILE

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	DefaultComposeFile = "docker-compose.yml"
+)
+
 func main() {
+	var composeFile string
+
+	if os.Getenv("COMPOSE_FILE") != "" {
+		composeFile = os.Getenv("COMPOSE_FILE")
+	} else {
+		composeFile = DefaultComposeFile
+	}
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 
@@ -25,19 +38,19 @@ func main() {
 		pubKey := c.PostForm("pubKey")
 
 		// libcompose does not support `docker-compose run`...
-		out, err := exec.Command("docker-compose", "run", "--rm", "gitreceive-upload-key", username, pubKey).Output()
+		out, err := exec.Command("docker-compose", "-f", composeFile, "run", "--rm", "gitreceive-upload-key", username, pubKey).Output()
 
 		if err != nil {
 			c.HTML(http.StatusInternalServerError, "index.tmpl", gin.H{
 				"alert":   true,
 				"error":   true,
-				"message": err.Error(),
+				"message": strings.Join([]string{"error: ", err.Error()}, ""),
 			})
 		} else {
 			c.HTML(http.StatusCreated, "index.tmpl", gin.H{
 				"alert":   true,
 				"error":   false,
-				"message": strings.Join([]string{"fingerprint: ", string(out)}, " "),
+				"message": strings.Join([]string{"fingerprint: ", string(out)}, ""),
 			})
 		}
 	})
