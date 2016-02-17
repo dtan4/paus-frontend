9a5f84858be7e27b3da7e6598f2d2ccd682d0db7
Daisuke Fujita <dtanshi45@gmail.com>
2016-02-17 17:50:06 +0900

    Create index page

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 	})
 
 	r.Run()
